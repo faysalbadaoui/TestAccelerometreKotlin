7a1fede8a58c0add5e4bf7eaf773f409ff303405
Faysal Badaoui <faysalbadaouimahdad@gmail.com>
2024-02-15 18:21:11 +0100

    apartat a done

--- app/src/main/java/com/example/testaccelerometrekotlin/AccelerometerViewModel.kt
+++ app/src/main/java/com/example/testaccelerometrekotlin/AccelerometerViewModel.kt
@@ -1,11 +1,32 @@
 package com.example.testaccelerometrekotlin
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class AccelerometerViewModel : ViewModel() {
     var state : MutableLiveData<String> = MutableLiveData("")
     fun changeValue(newValue: String){
         state.value = newValue
+    }
+
+    private val _midBoxText = MutableStateFlow<String>("")
+    val midBoxText : StateFlow<String> = _midBoxText.asStateFlow()
+
+    fun changeMidText(newValue: String){
+        _midBoxText.value = newValue
+    }
+
+    private val _boxColor = MutableStateFlow<Color>(Color.Green)
+    val boxColor : StateFlow<Color> = _boxColor.asStateFlow()
+
+    fun setBoxColor(newValue: Color){
+        _boxColor.value = newValue
     }
 }
--- app/src/main/java/com/example/testaccelerometrekotlin/TestAccelerometreActivity.kt
+++ app/src/main/java/com/example/testaccelerometrekotlin/TestAccelerometreActivity.kt
@@ -1,7 +1,7 @@
 package com.example.testaccelerometrekotlin
 
 import android.annotation.SuppressLint
-import android.graphics.Color
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -12,12 +12,19 @@ import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.testaccelerometrekotlin.databinding.MainBinding
 
 class TestAccelerometreActivity : AppCompatActivity(), SensorEventListener {
@@ -32,17 +39,48 @@ class TestAccelerometreActivity : AppCompatActivity(), SensorEventListener {
     @SuppressLint("CoroutineCreationDuringComposition")
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        sensorManager.registerListener(
-            this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
+        val hasAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+        if(hasAccelerometer){
+            viewModel.changeMidText("Shake to get a toast and to switch color")
+            sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }else{
+            viewModel.changeMidText("Sorry there Is no Accelerometer in your device")
+        }
+
         lastUpdate = System.currentTimeMillis()
         setContent {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(text = viewModel.state.observeAsState().value!!)
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Box(modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .background(
+                            viewModel.boxColor.collectAsState().value
+                        ), contentAlignment = Alignment.Center) {
+                        Text(text ="1")
+                    }
+                    Box(modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .background(
+                            androidx.compose.ui.graphics.Color.Red
+                        ), contentAlignment = Alignment.Center) {
+                        Text(text = viewModel.midBoxText.collectAsState().value)
+                    }
+                    Box(modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .background(
+                            androidx.compose.ui.graphics.Color.Blue
+                        ), contentAlignment = Alignment.Center) {
+                        Text(text ="3")
+                    }
+                }
             }
         }
     }
@@ -60,18 +98,16 @@ class TestAccelerometreActivity : AppCompatActivity(), SensorEventListener {
         val accelerationSquareRoot = (x * x + y * y + z * z
                 / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH))
         val actualTime = System.currentTimeMillis()
-        if (accelerationSquareRoot >= 200) {
+        if (accelerationSquareRoot >= 100) {
             if (actualTime - lastUpdate < 1000) {
                 return
             }
             lastUpdate = actualTime
+            if(viewModel.boxColor.value == Color.Green)
+                viewModel.setBoxColor(Color.Cyan)
+            else
+                viewModel.setBoxColor(Color.Green)
             Toast.makeText(this, R.string.shuffed, Toast.LENGTH_SHORT).show()
-            if (color) {
-                view.setBackgroundColor(Color.GREEN)
-            } else {
-                view.setBackgroundColor(Color.RED)
-            }
-            color = !color
         }
     }
 
